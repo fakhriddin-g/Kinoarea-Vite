12c9bb7315f778e4299efa17d452f871b57e35e2
Fakhriddin <fakhmirakov1998@gmail.com>
2023-07-27 01:04:56 +0500

    some changes

--- modules/interface.js
+++ modules/interface.js
@@ -75,3 +75,132 @@ export function otherPopularActerFunction(arr, place) {
     `
   }
 }
+
+export function movieCardFunction(arr, place) {
+
+  console.log(arr);
+  let body = document.body
+  body.style.backgroundImage = `url('${import.meta.env.VITE_BASE_IMG + arr.backdrop_path}')`
+  body.backgroundSize = 'cover'
+  place.innerHTML += `
+
+    <div class="container">
+      <div class="movie-card__top">
+        <div class="movie-card__img">
+          <img src="${import.meta.env.VITE_BASE_IMG + arr.poster_path}" alt="">
+        </div>
+
+        <div class="movie-card__top-right">
+          <div class="movie-card__top-path">Главная > Фильмы > <span>${arr.original_title}</span></div>
+          <h3 class="movie-card__top-original-name">${arr.original_title}</h3>
+          <span class="movie-card__top-title">${arr.title}</span>
+          <div class="movie-card__top-chart-rating">
+            <div class="circle">1
+              <span>Kinoarea</span>
+            </div>
+            <div class="circle">2
+              <span>IMDb</span>
+            </div>
+          </div>
+          <p class="movie-card__top-desc">${arr.overview}</p>
+          <div class="movie-card__top-trailer">
+            <button>Смотреть трейлер</button>
+            <img src="/public/images/social-net.png" alt="">
+          </div>
+        </div>
+      </div>
+
+      <div class="movie-card__bottom">
+        <div class="movie-card__bottom-rating-row">
+          <img src="/public/icons/like.svg" alt="" class="like">
+          <img src="/public/icons/dislike.svg" alt="" class="dislike">
+          <div class="rating-box">
+            <div class="rating-box-text"></div>
+          </div>
+          <img src="/public/icons/heart.svg" alt="" class="heart">
+          <div class="bookmark">В избранном у 37933 человек </div>
+        </div>
+        <div class="movie-card__bottom-about">
+          <div class="left">
+            <table>
+              <tbody>
+                <tr>
+                    <td>Год:</td>
+                    <td class="color-column">${arr.release_date.split("-").at(0)}</td>
+                </tr>
+                <tr>
+                    <td>Страна:</td>
+                    <td class="color-column">${arr.production_countries[0].name}</td>
+                </tr>
+                <tr>
+                    <td>Слоган:</td>
+                    <td class="color-column">${arr.tagline}</td>
+                </tr>
+                <tr>
+                    <td>Режиссер:</td>
+                    <td class="color-column">Фрэнсис Аннан</td>
+                </tr>
+                <tr>
+                    <td>Сценарий:  </td>
+                    <td class="color-column">Фрэнсис Аннан, Л.Х. Адамс, Кэрол Гриффитс, ...</td>
+                </tr>
+                <tr>
+                    <td>Продюсер:</td>
+                    <td class="color-column">Дэвид Баррон, Марк Блэйни, Гари Хэмилтон, ...</td>
+                </tr>
+                <tr>
+                    <td>Оператор:</td>
+                    <td class="color-column">Джеффри Холл</td>
+                </tr>
+                <tr>
+                    <td>Композитор:</td>
+                    <td class="color-column">Дэвид Хиршфелдер</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <div class="right">
+            <table>
+              <tbody>
+                <tr>
+                  <td>Художник: </td>
+                  <td class="color-column">Скотт Бёрд, Эрика Брайан, Мариот Керр, ... </td>
+                </tr>
+                <tr>
+                  <td>Монтаж: </td>
+                  <td class="color-column">Ник Фентон </td>
+                </tr>
+                <tr>
+                  <td> Жанр: </td>
+                  <td class="color-column">Триллер, ...</td>
+                </tr>
+                <tr>
+                  <td>Сборы в мире: </td>
+                  <td class="color-column">${arr.budget + ' ' + '$'}</td>
+                </tr>
+                <tr>
+                  <td>Премьера (мир): </td>
+                  <td class="color-column">${arr.release_date}</td>
+                </tr>
+                <tr>
+                  <td>Премьера (РФ): </td>
+                  <td class="color-column">${arr.release_date}</td>
+                </tr>
+                <tr>
+                  <td>Возраст:</td>
+                  <td class="color-column">16+ </td>
+                </tr>
+                <tr>
+                  <td>Время: </td>
+                  <td class="color-column">${arr.runtime}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+  `
+}
--- pages/film-card/main.js
+++ pages/film-card/main.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { headerFunction } from "../../modules/header";
+import { movieCardFunction } from "../../modules/interface";
 
 let movieCard = document.querySelector('.movie-card')
 
@@ -12,42 +13,6 @@ axios.get(`https://api.themoviedb.org/3/movie/${movie_id}`, {
     Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
     Accept: 'application/json'
   }
-}).then(res => movieCardFunction(res.data))
+}).then(res => movieCardFunction(res.data, movieCard))
 
-function movieCardFunction(arr) {
-
-  console.log(arr);
-  movieCard.innerHTML += `
-
-    <div class="container">
-      <div class="movie-card__top">
-        <div class="movie-card__img">
-          <img src="${import.meta.env.VITE_BASE_IMG + arr.poster_path}" alt="">
-        </div>
-
-        <div class="movie-card__top-right">
-          <div class="movie-card__top-path">Главная > Фильмы > <span>${arr.original_title}</span></div>
-          <h3 class="movie-card__top-original-name">${arr.original_title}</h3>
-          <span class="movie-card__top-title">${arr.title}</span>
-          <div class="movie-card__top-chart-rating">
-            <div class="circle">1
-              <span>Kinoarea</span>
-            </div>
-            <div class="circle">2
-              <span>IMDb</span>
-            </div>
-          </div>
-          <p class="movie-card__top-desc">${arr.overview}</p>
-          <div class="movie-card__top-trailer">
-            <button>Смотреть трейлер</button>
-            <img src="/public/images/social-net.png" alt="">
-          </div>
-        </div>
-      </div>
-
-      <div class="movie-card__bottom"></div>
-    </div>
-  
-  `
-  
-}
+// movieCardFunction([], movieCard)
