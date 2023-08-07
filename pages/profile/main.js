6069231e7efda206c4dd3224bbfdb38891e9a461
Fakhriddin <fakhmirakov1998@gmail.com>
2023-08-07 18:08:38 +0500

    profile satrt

--- pages/profile/main.js
+++ pages/profile/main.js
@@ -1,3 +1,28 @@
 import { headerFunction } from "../../modules/header";
 
 headerFunction()
+
+let containers = document.querySelectorAll('main .wrapper')
+let tabs = document.querySelectorAll('aside img')
+
+containers.forEach((container, idx) => {
+  if (idx !== 0) {
+    container.classList.add('hide')
+  }
+})
+
+tabs.forEach((btn) => {
+  let key = btn.id;
+
+  btn.onclick = () => {
+     tabs.forEach(btn => btn.classList.remove("active_link"))
+     btn.classList.add("active_link")
+     if (key !== "ellipse" && key !== "out") {
+
+        containers.forEach(cont => cont.classList.add('hide'))
+
+        let cont = document.querySelector(`#cont-${key}`)
+        cont.classList.remove('hide')
+     }
+  }
+})
